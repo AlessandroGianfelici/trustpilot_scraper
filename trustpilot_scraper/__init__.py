adf82bbfcc75bccd7d9b231773622abacb8c5572
Alessandro Gianfelici <alessandro.gianfelici@hotmail.com>
2021-05-18 16:52:27 +0200

    codice reso scrapy-ready

--- trustpilot_scraper/__init__.py
+++ trustpilot_scraper/__init__.py
@@ -5,10 +5,8 @@ Created on Thu Aug 30 15:04:00 2018
 @author: Alessandro Gianfelici
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
-import bs4
-from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 from langdetect import detect
@@ -16,15 +14,7 @@ from langdetect import detect
 myLongNamedDictionary = {}
 null = None
 
-def getNumPages(url):
-    response = get(url+'?languages=all&page=1')
-    html_soup = BeautifulSoup(response.text, 'html.parser') 
-    tmp = html_soup.find_all('span', class_ = 'headline__review-count')
-    NrevTot = int(tmp[0].text)
-    Nrec1pag = len(html_soup.find_all('div', class_ = 'review-card'))
-    return NrevTot//Nrec1pag + 1
-
-def scrapeTrustPilot(input_url : str):
+def scrapeTrustPilot(body : str):
     """
     This function take the url of the trustpilot page of the company you are interested in 
     and return as output a pandas dataframe containing the following columns:
@@ -40,19 +30,14 @@ def scrapeTrustPilot(input_url : str):
     testo = []
     stelle = []
     lingua = []
-    nPages = getNumPages(input_url)
-    for page in range(1,nPages):
-        print(f"Processing page {page} of {nPages-1}...")
-        url = f'{input_url}?languages=all&page={page}'
-        response = get(url)
-        html_soup = BeautifulSoup(response.text, 'html.parser')
-        recensioni = html_soup.find_all('div', class_ = 'review-card')
-        for recensione in recensioni:
-            exec(f"global myLongNamedDictionary; global null; myLongNamedDictionary = {recensione.script.contents[0][1:-1]}")
-            titolo.append(myLongNamedDictionary['reviewHeader'])
-            testo.append(myLongNamedDictionary['reviewBody'])
-            stelle.append(myLongNamedDictionary['stars'])
-            lingua.append(detect(myLongNamedDictionary['reviewBody']))
+    html_soup = BeautifulSoup(body, 'html.parser')
+    recensioni = html_soup.find_all('div', class_ = 'review-card')
+    for recensione in recensioni:
+        exec(f"global myLongNamedDictionary; global null; myLongNamedDictionary = {recensione.script.contents[0][1:-1]}")
+        titolo.append(myLongNamedDictionary['reviewHeader'])
+        testo.append(myLongNamedDictionary['reviewBody'])
+        stelle.append(myLongNamedDictionary['stars'])
+        lingua.append(detect(myLongNamedDictionary['reviewBody']))
     data = {'company_name' : myLongNamedDictionary['businessUnitDisplayName'],
             'review_title' : titolo,
             'review_text' : testo,
